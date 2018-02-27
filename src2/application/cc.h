d3328593d2702936b262d94344203afea976e097
yxtj <yanxiangtianji@gmail.com>
2018-02-27 21:21:20 +0800

    small update

--- src2/application/cc.h
+++ src2/application/cc.h
@@ -1,35 +1,24 @@
 #include "common/Kernel.h"
-#include "common/io.h"
 #include <numeric>
 #include <string>
 //#include <algorithm>
-
-using namespace std;
 
 struct ConnectedComponent
 	: public Kernel<key_t, key_t> 
 {
 	bool pri_degree;
 	bool pri_delta;
-	virtual bool parse(const std::vector<std::string>& arg_line){}
+	virtual bool parse(const std::vector<std::string>& arg_line);
 
-	virtual std::pair<key_t, neighbor_list_t> load_graph(std::string& line){
-		return common::load_graph_unweighted(line);
-	}
-	virtual change_t load_change(std::string& line){
-		return common::load_change(line);
-	}
-	virtual std::pair<key_t, value_t> load_value(std::string& line){
-		return common::load_value(line);
-	}
+	virtual std::pair<key_t, neighbor_list_t> load_graph(std::string& line);
+	virtual change_t load_change(std::string& line);
+	virtual std::pair<key_t, value_t> load_value(std::string& line);
 	
 	// initialize the starting value
-	virtual value_t init_value(const key_t& k, const neighbor_list_t& neighbors){
-		return k;
-	}
+	virtual value_t init_value(const key_t& k, const neighbor_list_t& neighbors);
 
 	virtual const value_t identity_element() const{
-		return numeric_limits<value_t>::min();
+		return std::numeric_limits<value_t>::min();
 	}
 	virtual value_t oplus(value_t& a, const value_t& b){
 		//return max(a, b);
@@ -49,8 +38,3 @@ struct ConnectedComponent
 		return n.v;
 	}
 };
-
-bool ConnectedComponent::parse(const std::vector<std::string>& arg_line){
-
-	return true;
-}
--- src2/runner/main.cpp
+++ src2/runner/main.cpp
@@ -1,16 +1,27 @@
 #include <iostream>
 #include <string>
 #include "Option.h"
+#include "network/NetworkThread.h"
 
 using namespace std;
 
 int main(int argc, char* argv[]){
-    Option opt;
-    if(!opt.parseInput(argc, argv)){
-        cerr<<"Failed in parsing arguments"<<endl;
-        return 1;
-    }
+	Option opt;
+	if(!opt.parseInput(argc, argv)){
+		cerr<<"Failed in parsing arguments"<<endl;
+		return 1;
+	}
+	NetworkThread::Init(argc, argv);
+	if(opt.show){
 
+	}
 
-    return 0;
+	NetworkThread* net = NetworkThread::GetInstance();
+	if(net->id() == 0){ // master
+
+	}else{ // worker
+		
+	}
+
+	return 0;
 }
