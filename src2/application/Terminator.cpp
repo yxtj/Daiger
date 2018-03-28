6cfb581a6550de32ed7a7902def9a0ec86b494cd
yxtj <yanxiangtianji@gmail.com>
2018-03-28 20:14:17 +0800

    optimize the structure of terminators

--- src2/application/Terminator.cpp
+++ src2/application/Terminator.cpp
@@ -1,7 +1,7 @@
 #include "Terminator.h"
 #include <cmath>
 #include <stdexcept>
-//#include <algorithm>
+#include <algorithm>
 
 using namespace std;
 
@@ -31,4 +31,57 @@ bool TerminatorBase::helper_no_change(const std::vector<ProgressReport>& reports
 			return false;
 	}
 	return true;
+}
+
+// -------- TerminatorStop --------
+
+void TerminatorStopBase::prepare_global_checker(const size_t n_worker){
+	TerminatorBase::prepare_global_checker(n_worker);
+	last.resize(n_worker);
+	sum_gc_last=0;
+	untouched = true;
+}
+
+void TerminatorStopBase::update_report(const size_t wid, const ProgressReport& report){
+	untouched = false;
+	sum_gc_last += TerminatorBase::curr[wid].n_change - last[wid];
+	last[wid] = TerminatorBase::curr[wid].n_change;
+	TerminatorBase::update_report(wid, report);
+}
+
+bool TerminatorStopBase::check_term(){
+	return !untouched && TerminatorBase::sum_gc == 0 && sum_gc_last == 0;
+}
+
+// -------- TerminatorDiff --------
+
+void TerminatorDiffBase::init(const std::vector<std::string>& args){
+	try{
+		epsilon = stod(args[0]);
+	} catch (exception& e){
+		throw invalid_argument("Unable to get <epsilon> for TerminatorDiff.");
+	}
+}
+
+void TerminatorDiffBase::prepare_global_checker(const size_t n_worker){
+	TerminatorBase::prepare_global_checker(n_worker);
+	last.resize(n_worker);
+	sum_gp_last=0.0;
+	sum_gi_last=0;
+	untouched = true;
+}
+
+void TerminatorDiffBase::update_report(const size_t wid, const ProgressReport& report){
+	untouched = false;
+	sum_gp_last += TerminatorBase::curr[wid].sum - last[wid].first;
+	sum_gi_last += TerminatorBase::curr[wid].n_inf - last[wid].second;
+	last[wid].first = TerminatorBase::curr[wid].sum;
+	last[wid].second = TerminatorBase::curr[wid].n_inf;
+	TerminatorBase::update_report(wid, report);
+}
+
+bool TerminatorDiffBase::check_term(){
+	return !untouched && TerminatorBase::sum_gc == 0
+		&& sum_gi_last == TerminatorBase::sum_gi
+		&& fabs(sum_gp_last - TerminatorBase::sum_gp) < epsilon;
 }
--- src2/application/Terminator.h
+++ src2/application/Terminator.h
@@ -3,8 +3,6 @@
 #include <vector>
 #include <string>
 #include <utility>
-#include <stdexcept>
-#include <cmath>
 #include <limits>
 
 struct ProgressReport{
@@ -42,14 +40,12 @@ protected:
 
 	std::vector<ProgressReport> curr;
 	double sum_gp; // sum global progress
-	double sum_gi; // sum global number of infinity
+	size_t sum_gi; // sum global number of infinity
 	size_t sum_gc; // sum global number of changes
 };
 
 template <typename V, typename N>
-class Terminator
-	: public TerminatorBase
-{
+class TerminatorTypedInterface {
 public:
 	// on workers:
 	// get the progress of a single node, return INF for special nodes/values
@@ -64,11 +60,36 @@ protected:
 	}
 };
 
-// -------- an example of a difference-based terminator --------
+template <typename V, typename N>
+class Terminator
+	: public TerminatorBase, public TerminatorTypedInterface<V, N>
+{};
+
+// -------- an example which stops when no one changes --------
+
+class TerminatorStopBase
+	: public TerminatorBase
+{
+public:
+	virtual void prepare_global_checker(const size_t n_worker);
+	virtual void update_report(const size_t wid, const ProgressReport& report);
+	virtual bool check_term();
+	
+private:
+	std::vector<size_t> last;
+	size_t sum_gc_last;
+	bool untouched;
+};
 
 template <typename V, typename N>
-class TerminatorDiff
-	: public Terminator<V, N>
+class TerminatorStop
+	: public TerminatorStopBase, public TerminatorTypedInterface<V, N>
+{};
+
+// -------- an example of a difference-based terminator --------
+
+class TerminatorDiffBase
+	: public TerminatorBase
 {
 public:
 	virtual void init(const std::vector<std::string>& args);
@@ -85,68 +106,6 @@ private:
 };
 
 template <typename V, typename N>
-void TerminatorDiff<V, N>::init(const std::vector<std::string>& args){
-	try{
-		epsilon = std::stod(args[0]);
-	} catch (std::exception& e){
-		throw std::invalid_argument("Unable to get <epsilon> for TerminatorDiff.");
-	}
-}
-template <typename V, typename N>
-void TerminatorDiff<V, N>::prepare_global_checker(const size_t n_worker){
-	TerminatorBase::prepare_global_checker(n_worker);
-	last.resize(n_worker);
-	sum_gp_last=0.0;
-	sum_gi_last=0;
-	untouched = true;
-}
-template <typename V, typename N>
-void TerminatorDiff<V, N>::update_report(const size_t wid, const ProgressReport& report){
-	untouched = false;
-	sum_gp_last += TerminatorBase::curr[wid].sum - last[wid].first;
-	sum_gi_last += TerminatorBase::curr[wid].n_inf - last[wid].second;
-	last[wid].first = TerminatorBase::curr[wid].sum;
-	last[wid].second = TerminatorBase::curr[wid].n_inf;
-	TerminatorBase::update_report(wid, report);
-}
-template <typename V, typename N>
-bool TerminatorDiff<V, N>::check_term(){
-	return !untouched && TerminatorBase::sum_gc == 0
-		&& sum_gi_last == TerminatorBase::sum_gi && fabs(sum_gp_last - TerminatorBase::sum_gp) < epsilon;
-}
-
-// -------- an example which stops when no one changes --------
-
-template <typename V, typename N>
-class TerminatorStop
-	: public Terminator<V, N>
-{
-public:
-	virtual void prepare_global_checker(const size_t n_worker);
-	virtual void update_report(const size_t wid, const ProgressReport& report);
-	virtual bool check_term();
-	
-private:
-	std::vector<size_t> last;
-	size_t sum_gc_last;
-	bool untouched;
-};
-
-template <typename V, typename N>
-void TerminatorStop<V, N>::prepare_global_checker(const size_t n_worker){
-	TerminatorBase::prepare_global_checker(n_worker);
-	last.resize(n_worker);
-	sum_gc_last=0;
-	untouched = true;
-}
-template <typename V, typename N>
-void TerminatorStop<V, N>::update_report(const size_t wid, const ProgressReport& report){
-	untouched = false;
-	sum_gc_last += TerminatorBase::curr[wid].n_change - last[wid];
-	last[wid] = TerminatorBase::curr[wid].n_change;
-	TerminatorBase::update_report(wid, report);
-}
-template <typename V, typename N>
-bool TerminatorStop<V, N>::check_term(){
-	return !untouched && TerminatorBase::sum_gc == 0 && sum_gc_last == 0;
-}
+class TerminatorDiff
+	: public TerminatorDiffBase, public TerminatorTypedInterface<V, N>
+{};
