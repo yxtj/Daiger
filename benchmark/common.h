e2b2e367ee3fced20608e433e21a6169c0e3479b
yxtj <yanxiangtianji@gmail.com>
2018-08-28 23:35:27 +0800

    add comment

--- benchmark/common.h
+++ benchmark/common.h
@@ -25,10 +25,11 @@ std::vector<std::vector<Edge>> general_load_weight(const int npart,
 std::vector<std::vector<int>> general_load_unweight(const int npart,
 	const std::string& gfolder, const std::string& gprefix, const std::string& dfolder, const std::string& dprefix);
 
-
+// load a graph file
 bool load_graph_weight(std::vector<std::vector<Edge>>& res, const std::string& fn);
 bool load_graph_unweight(std::vector<std::vector<int>>& res, const std::string& fn);
 
+// merge delta information into a graph
 bool merge_graph_weight(std::vector<std::vector<Edge>>& res, const std::string& fn);
 bool merge_graph_unweight(std::vector<std::vector<int>>& res, const std::string& fn);
 
